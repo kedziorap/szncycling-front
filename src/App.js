f866226f60a5a8688778497ec3cdc82772eed6ff
Piotr Kedziora <piotrkedziorapl@gmail.com>
2022-02-22 14:05:10 +0100

    added React.lazy

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Tracks from './pages/Tracks';
-import OneTrack from './pages/OneTrack';
-import NotFound from './pages/NotFound';
-function App() {
+import Loader from './components/UI/Loader';
 
+const Home = React.lazy(()=>import('./pages/Home'));
+const Contact = React.lazy(()=>import('./pages/Contact'));
+const About = React.lazy(()=>import('./pages/About'));
+const Tracks = React.lazy(()=>import('./pages/Tracks'));
+const OneTrack = React.lazy(()=>import('./pages/OneTrack'));
+const NotFound = React.lazy(()=>import('./pages/NotFound'));
+
+function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/tracks" exact>
-          <Tracks />
-        </Route>
-        <Route path="/tracks/:id" exact>
-          <OneTrack />
-        </Route>
-        <Route path="/about" exact>
-          <About />
-        </Route>
-        <Route path="/contact" exact>
-          <Contact />
-        </Route>
-        <Route path="/*" exact>
-          <NotFound/>
-        </Route>
-      </Switch>
-
+      <Suspense fallback={<Loader/>}>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/tracks" exact>
+            <Tracks />
+          </Route>
+          <Route path="/tracks/:id" exact>
+            <OneTrack />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Route path="/*" exact>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
