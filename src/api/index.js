e9551b7f5f9d6701c331a76ecc86ac80e985fac6
Piotr Kedziora <piotrkedziorapl@gmail.com>
2022-03-02 08:52:42 +0100

    remove debug

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/v1';
+const API_URL = '/api/v1';
 
 const request = async (endpoint, method = 'GET', data = null) => {
   const config = {
--- src/i18n.js
+++ src/i18n.js
@@ -21,7 +21,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'pl',
-    debug: true,
+   // debug: true,
     whitelist: Languages,
 
     interpolation: {
